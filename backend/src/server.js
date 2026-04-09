3c56b7392b8be818d010b5468a5ade0708dfd953
Nour <fllstknour7@gmail.com>
2026-04-09 13:21:01 +0200

    09APR26-ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -7,9 +8,20 @@ import messageRoutes from "./routes/message.route.js";
 dotenv.config();
 
 const app = express();
+const __direname = path.resolve();
+
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+/* Yalla Deployment */
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__direname, "../frontend/dist")));
+
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__direname, "../frontend/dist/index.html"));
+  });
+}
 
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
