ff71fb5215b64e5dbd6d35097e0df2a54bdc68f1
Nour <fllstknour7@gmail.com>
2026-04-14 18:23:09 +0200

    14MAR26 - welcome email implemented

---
+++ backend/src/lib/env.js
@@ -0,0 +1,25 @@
+import "dotenv/config";
+
+export const ENV = {
+  PORT: process.env.PORT,
+  DB_URL: process.env.DB_URL,
+  JWT_SECRET: process.env.JWT_SECRET,
+  NODE_ENV: process.env.NODE_ENV,
+  CLIENT_URL: process.env.CLIENT_URL,
+  RESEND_API_KEY: process.env.RESEND_API_KEY,
+  EMAIL_FROM: process.env.EMAIL_FROM,
+  EMAIL_FROM_NAME: process.env.EMAIL_FROM_NAME,
+};
+
+// PORT=5000
+
+// DB_URL = mongodb+srv://nourihab745_db_user:ihahuLTML2IyEGhq@chatny-cluster.wnir6tk.mongodb.net/chatny_db?appName=Chatny-Cluster
+
+// NODE_ENV = development
+
+// RESEND_API_KEY = re_gxbBEyqZ_3HDAx46X3V2KAt4FHbwuWC8Y
+// EMAIL_FROM="oncompiling@resend.dev"
+// EMAIL_FROM_NAME="Nour Ihab"
+// JWT_SECRET = myjwtsecret
+
+// CLIENT_URL = http://localhost:5173
