8f63fa2dbe075de13c0b6742120237e78338a3ca
Nour <fllstknour7@gmail.com>
2026-04-15 10:58:19 +0200

    15MAR26 - message routes added

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,18 @@
 import express from "express";
-
+import {
+  getAllContacts,
+  getChatPartners,
+  getMessagesByUserId,
+  sendMessage,
+} from "../controller/message.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { archjetProtection } from "../middleware/archjet.middleware.js";
 const router = express.Router();
+router.use(archjetProtection, protectRoute);
 
-router.get("/send", (req, res) => {
-  res.send("Send message endpoint");
-});
+router.get("/contacts", getAllContacts);
+router.get("/chats", getChatPartners);
+router.get("/:id", getMessagesByUserId);
+router.post("/send/:id", sendMessage);
 
 export default router;
