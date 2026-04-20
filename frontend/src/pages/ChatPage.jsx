3ff7c7ec72c8170d6c0166908907cf5fbecbc534
Nour <fllstknour7@gmail.com>
2026-04-20 19:03:24 +0200

    20APR26 -  login & logout

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,5 +1,13 @@
+import { useAuthStore } from "../store/useAuthStore";
+
 function ChatPage() {
-  return <div>ChatPage</div>;
+  const { logout } = useAuthStore();
+  return (
+    <div className="z-10">
+      ChatPage
+      <button onClick={logout}>logout</button>
+    </div>
+  );
 }
 
 export default ChatPage;
